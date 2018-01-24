6760a517026f6770d2ec04003b3e2488c022c6ea
Spencer <yeshaopeng@lianqin360.com>
2018-01-24 19:57:00 +0800

    storageProvider

--- app/Library/provider/StorageProvider.js
+++ app/Library/provider/StorageProvider.js
@@ -1,24 +1,42 @@
 'use strict'
 import Provider from './Provider';
 
-
-class StorageProvider extends Provider{
-    _synced:boolean;
-    constructor(config){
+var storageTool = {
+    getter: function () {
+        return Promise.resolve();
+    },
+    setter: function () {
+        return Promise.resolve();
+    }
+}
+class StorageProvider extends Provider {
+    _synced: boolean;
+    constructor(config) {
         super(config);
     }
-    get(){
-        if(!this._synced){
-
+    get() {
+        if (!this._synced) {
+            return storageTool.getter(this._name)
+                .then((v) => {
+                    if (v) {
+                        this._value = v.value;
+                    }
+                    this._synced = true;
+                    return super.get();
+                })
+        } else {
+            return super.get();
         }
     }
-    set(v){
+    set(v) {
         super.set(v);
+        storageTool.setter(this._name, { timestamp: Date.now(), value: v });
 
     }
 }
-function setStorgaeTool(){
-    
+function setStorgaeTool(tool) {
+    storageTool = tool;
 }
 
+StorageProvider.setStorgaeTool = setStorgaeTool;
 export default StorageProvider;
